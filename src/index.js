19791e205090468d112509c0ca8ab1951cc1cac9
Diogo Teixeira <teixeira.diogo5@gmail.com>
2017-04-01 10:48:31 +0100

    section 1 done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyB1QFIv7Dd01lgHUsSK5XFos4yML_LfY6s';
 
 const App = () => {
-    return <div><SearchBar/></div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('.container'));
